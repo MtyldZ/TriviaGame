d2ee343f988a5191bd2100c3096f205fd6275899
MtyldZ <56448237+MtyldZ@users.noreply.github.com>
2021-03-27 11:33:52 +0300

    FIX victory screen navigation fixed, custom back event added

--- src/screens/CorrectScreen/index.tsx
+++ src/screens/CorrectScreen/index.tsx
@@ -1,17 +1,23 @@
 import React, {memo, useCallback} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {HeaderComponent} from '../../components/HeaderComponent';
-import {incrementQuestionIndexAction, resetTriviaGameAction} from '../../store/triviaGame/action';
-import {StackActions, useFocusEffect, useNavigation} from '@react-navigation/native';
+import {incrementQuestionIndexAction, resetTriviaGameAction, setHighScoresAction} from '../../store/triviaGame/action';
+import {useFocusEffect, useNavigation} from '@react-navigation/native';
 import {Colors} from '../../utils/color';
 import {Alert, BackHandler, Image, Text, TouchableOpacity, View} from 'react-native';
 import {Styles} from './style';
+import {UserScore} from '../../utils/types';
 
 export const CorrectScreen = memo(() => {
     const dispatch = useDispatch();
     const navigation = useNavigation();
+    const questionIndex = useSelector(state => state.triviaGame.questionIndex);
     const earnedPoint = useSelector(state => state.triviaGame.lastEarnedPointAmount);
     const totalPoint = useSelector(state => state.triviaGame.totalPoint);
+    const category = useSelector(state => state.triviaGame.chosenCategory);
+    const difficulty = useSelector(state => state.triviaGame.chosenDifficulty);
+    const timeSpent = useSelector(state => state.triviaGame.totalTimeSpent);
+    const allScores = useSelector(state => state.triviaGame.highScores);
 
     const onBackRequestHandler = useCallback(() => {
         Alert.alert('Caution!', 'Are you sure you want give up?', [
@@ -20,7 +26,7 @@ export const CorrectScreen = memo(() => {
                 text: 'Give Up',
                 style: 'default',
                 onPress: () => {
-                    navigation.dispatch(StackActions.replace('Start'));
+                    navigation.navigate('Start');
                     dispatch(resetTriviaGameAction());
                 },
             },
@@ -28,17 +34,31 @@ export const CorrectScreen = memo(() => {
         return true;
     }, [dispatch, navigation]);
 
-    useFocusEffect(
-        useCallback(() => {
-            BackHandler.addEventListener('hardwareBackPress', onBackRequestHandler);
-            return () => BackHandler.removeEventListener('hardwareBackPress', onBackRequestHandler);
-        }, [onBackRequestHandler]));
+    useFocusEffect(useCallback(() => {
+        BackHandler.addEventListener('hardwareBackPress', onBackRequestHandler);
+        return () => BackHandler.removeEventListener('hardwareBackPress', onBackRequestHandler);
+    }, [onBackRequestHandler]));
 
-    const pressHandler = useCallback(
-        () => {
-            dispatch(incrementQuestionIndexAction());
-            navigation.dispatch(StackActions.replace('Question'));
-        }, [dispatch, navigation]);
+    const pressHandler = useCallback(() => {
+        if (questionIndex === 10) {
+
+            const score: UserScore = {
+                totalTimeSpent: timeSpent,
+                category: category,
+                difficulty: difficulty,
+                score: totalPoint,
+            };
+            const tempArr = [...allScores, score].sort((a, b) => (
+                b.score - a.score));
+
+            dispatch(setHighScoresAction(tempArr));
+            navigation.navigate('Start');
+            dispatch(resetTriviaGameAction());
+
+        }
+        dispatch(incrementQuestionIndexAction());
+        navigation.navigate('Question');
+    }, [allScores, category, difficulty, dispatch, navigation, questionIndex, timeSpent, totalPoint]);
 
     return (
         <>
--- src/screens/VictoryScreen/index.tsx
+++ src/screens/VictoryScreen/index.tsx
@@ -1,36 +1,31 @@
 import React, {memo, useCallback} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {resetTriviaGameAction, setHighScoresAction} from '../../store/triviaGame/action';
-import {UserScore} from '../../utils/types';
+import {resetTriviaGameAction} from '../../store/triviaGame/action';
 import {HeaderComponent} from '../../components/HeaderComponent';
-import {useNavigation} from '@react-navigation/native';
+import {useFocusEffect, useNavigation} from '@react-navigation/native';
 import {Colors} from '../../utils/color';
 import {Styles} from '../TimeOutScreen/style';
-import {Image, Text, TouchableOpacity, View} from 'react-native';
+import {BackHandler, Image, Text, TouchableOpacity, View} from 'react-native';
 
 export const VictoryScreen = memo(() => {
     const dispatch = useDispatch();
     const navigation = useNavigation();
     const totalPoint = useSelector(state => state.triviaGame.totalPoint);
-    const category = useSelector(state => state.triviaGame.chosenCategory);
-    const difficulty = useSelector(state => state.triviaGame.chosenDifficulty);
-    const timeSpent = useSelector(state => state.triviaGame.totalTimeSpent);
-    const allScores = useSelector(state => state.triviaGame.highScores);
 
-    const pressHandler = useCallback(() => {
-        const score: UserScore = {
-            totalTimeSpent: timeSpent,
-            category: category,
-            difficulty: difficulty,
-            score: totalPoint,
-        };
-        const tempArr = [...allScores, score].sort((a, b) => (
-            b.score - a.score));
-
-        dispatch(setHighScoresAction(tempArr));
+    const buttonPressEventHandler = useCallback(() => {
         navigation.navigate('Start');
         dispatch(resetTriviaGameAction());
-    }, [allScores, category, difficulty, dispatch, navigation, timeSpent, totalPoint]);
+    }, [dispatch, navigation]);
+
+    const hardwareBackPressEventHandler = useCallback(() => {
+        buttonPressEventHandler();
+        return true;
+    }, [buttonPressEventHandler]);
+
+    useFocusEffect(useCallback(() => {
+        BackHandler.addEventListener('hardwareBackPress', hardwareBackPressEventHandler);
+        return () => BackHandler.removeEventListener('hardwareBackPress', hardwareBackPressEventHandler);
+    }, [hardwareBackPressEventHandler]));
 
     return (
         <>
@@ -47,7 +42,7 @@ export const VictoryScreen = memo(() => {
                     </Text>
                 </View>
                 <TouchableOpacity style={Styles.buttonStyle}
-                                  onPress={pressHandler}>
+                                  onPress={buttonPressEventHandler}>
                     <Text style={Styles.smallerText}>{'Main Menu'}</Text>
                 </TouchableOpacity>
             </View>
